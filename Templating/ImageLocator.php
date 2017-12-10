14e817cd4600c80add82c68088e544e6c699cfce
anboto <anboto@gmail.com>
2017-12-10 13:21:04 +0100

    Now search image not only in *images* folder inside Resources/public but also search inside *img* folder Remove deprecations from the example's config Added teamshirts pdf module Bump framework-extra-bundle Symfony 4 support

--- Templating/ImageLocator.php
+++ Templating/ImageLocator.php
@@ -23,31 +23,34 @@ class ImageLocator implements ImageLocatorInterface
     {
         $this->kernel = $kernel;
     }
-    
+
     /**
      * Converts image logical name in "BundleName:image-name.extension" format to absolute file path.
-     * 
+     *
+     * @param string $logicalImageName
+     * @param string $publicSubDir
+     *
      * @return string file path
-     * 
+     *
      * @throws /InvalidArgumentException If bundle does not exist.
      */
-    public function getImagePath($logicalImageName)
+    public function getImagePath($logicalImageName, $publicSubDir = '/Resources/public')
     {
         $pos = strpos($logicalImageName, ':');
+        $imagesSubDir = is_dir($publicSubDir.'/images') ? $publicSubDir.'/images/' : $publicSubDir.'/img/';
 
         // add support for ::$imagePath syntax as in twig
         // @see http://symfony.com/doc/current/book/page_creation.html#optional-step-3-create-the-template
-        if($pos === false || $pos === 0)
-        {
-            return $this->kernel->getRootDir() . '/Resources/public/images/' . ltrim($logicalImageName, ':');
+        if ($pos === false || $pos === 0) {
+            return $this->kernel->getRootDir().$imagesSubDir.ltrim($logicalImageName, ':');
         }
 
-        $bundleName = substr($logicalImageName, 0, $pos);  
+        $bundleName = substr($logicalImageName, 0, $pos);
         $imageName = substr($logicalImageName, $pos + 1);
-        
+
         $bundle = $this->kernel->getBundle($bundleName);
         $bundlePath = $bundle->getPath();
-        
-        return $bundlePath.'/Resources/public/images/'.$imageName;
+
+        return $bundlePath.$imagesSubDir.$imageName;
     }
 }
